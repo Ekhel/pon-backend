167495fb3bb746527707afa780c8db3bbfa4cd5c
ekhel <blackmanskill@gmail.com>
2019-12-01 16:38:32 +0900

    Whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -60,9 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'backend.urls'
 
