117e993a62b5968cb041993000cfedb29ec11c56
ekhel <blackmanskill@gmail.com>
2019-12-01 16:19:56 +0900

    Seting Secret

--- backend/settings.py
+++ backend/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5l1agajt8e!x%*u7algcn6(2q2^b!!=9kwq=7k(i(f-bf9h6#r'
+# SECRET_KEY = '5l1agajt8e!x%*u7algcn6(2q2^b!!=9kwq=7k(i(f-bf9h6#r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','pon-backend.herokuapp.com']
 
 
 # Application definition
---
+++ ~/.bash_profile
@@ -0,0 +1,4 @@
+# Django Admin VARS
+
+export SECRET_KEY="d8d9aeb0ced3328e64de921c3e3c17217226c103baed2f59"
+export DEBUG_VALUE="True"
