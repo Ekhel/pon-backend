3c3a41c77b0eac390fa868c8876243a2c06cd428
ekhel <blackmanskill@gmail.com>
2019-12-01 16:35:11 +0900

    Settings

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,10 @@ ACCOUNT_EMAIL_REQUIRED = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
