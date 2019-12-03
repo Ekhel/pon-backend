25382bcf49f5584efb7a516e323e513816a3a836
ekhel <blackmanskill@gmail.com>
2019-12-03 11:47:25 +0900

    Page

--- pon/urls.py
+++ pon/urls.py
@@ -4,8 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from . views import (
     KotaView,
+    KatergoriKulinerView,
     KulinerView,
+    ItemKulinerView,
+    KategoriPenginapanView,
     PenginapanView,
+    ItemKamarView,
 )
 
 from . import views
@@ -16,6 +20,10 @@ urlpatterns = [
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('kota', login_required(KotaView.as_view()), name='kota'),
+    path('kategori_kuliner', login_required(KatergoriKulinerView.as_view()), name='kategori_kuliner'),
     path('kuliner', login_required(KulinerView.as_view()), name='kuliner'),
+    path('item_kuliner', login_required(ItemKulinerView.as_view()), name='item_kuliner'),
+    path('kategori_penginapan', login_required(KategoriPenginapanView.as_view()), name='kategori_penginapan'),
     path('penginapan', login_required(PenginapanView.as_view()), name='penginapan'),
+    path('item_kamar', login_required(ItemKamarView.as_view()), name='item_kamar'),
 ]
--- pon/views.py
+++ pon/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.generic import ListView
-from .models import Kota, Kuliner, Penginapan
+from .models import (
+    Kota, 
+    Kuliner, 
+    Kategori_kuliner,
+    Item_Kuliner,
+    Kategori_penginapan,
+    Penginapan,
+    ItemKamar
+)
 
 def login(request):
     return render(request,'auth/login.html')
@@ -16,12 +24,33 @@ class KotaView(ListView):
     template_name = 'layout/r-kota.html';
     model = Kota
 
+# Kuliner
+
+class KatergoriKulinerView(ListView):
+    template_name = 'layout/r-kategori-kuliner.html';
+    model = Kategori_kuliner
+
 class KulinerView(ListView):
     template_name = 'layout/r-kuliner.html';
     model = Kuliner
 
+class ItemKulinerView(ListView):
+    template_name = 'layout/r-item-kuliner.html';
+    model = Item_Kuliner
+
+# End Kuliner
+
+# Penginapan
+class KategoriPenginapanView(ListView):
+    template_name = 'layout/r-kategori-penginapan.html';
+    model = Kategori_penginapan
+
 class PenginapanView(ListView):
     template_name = 'layout/r-penginapan.html';
     model = Penginapan
+
+class ItemKamarView(ListView):
+    template_name ='layout/r-item-kamar.html';
+    model = ItemKamar
 
 
